4e76acc618240f039de7696333c5675ae96b5f6b
Chirag <chiragbuddy5@gamil.com>
2025-05-03 18:24:56 +0530

    mongodb secret moved to env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URL);
+  await mongoose.connect(
+    "mongodb+srv://Chiragvk:" +
+      process.env.MONGODB_PASSWORD +
+      "@cluster0.w9kw428.mongodb.net/socialMedia"
+  );
 };
 
 module.exports = connectDB;
