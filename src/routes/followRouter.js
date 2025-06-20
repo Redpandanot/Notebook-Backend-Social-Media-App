f7fa4a7fcbf1b9abfc0c223448b8e9eaa9818965
Chirag <chiragbuddy5@gamil.com>
2025-06-20 23:05:04 +0530

    added image optimization to every endpoint and added logs to the endpoints to track the changes in the log

--- src/routes/followRouter.js
+++ src/routes/followRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const followRouter = express.Router();
 const userAuth = require("../middlewares/userAuth");
 const Followers = require("../models/followers");
+const { optimizedImg } = require("../utils/helperFunctions");
 
 followRouter.get("/followers", userAuth, async (req, res) => {
   try {
@@ -11,14 +12,45 @@ followRouter.get("/followers", userAuth, async (req, res) => {
     })
       .populate({
         path: "followee",
-        select: "firstName lastName photo",
+        select: "firstName lastName",
       })
       .populate({
         path: "follower",
         select: "firstName lastName photo",
       })
       .lean();
-    res.send(followers);
+    const imgOptimizedFriendsList = followers.map((item) => {
+      if (
+        !item.follower ||
+        !item.follower.photo ||
+        !item.follower.photo.public_id
+      ) {
+        console.log("images not optmizied as needed info was missing");
+        return item;
+      }
+
+      console.log("begin optmizing images");
+
+      const optimizedProfileImg = optimizedImg(
+        item.follower.photo.public_id,
+        50
+      );
+
+      console.log("Image optimized!");
+
+      return {
+        ...item,
+        follower: {
+          ...item.follower,
+          photo: {
+            ...item.follower.photo,
+            url: optimizedProfileImg,
+          },
+        },
+      };
+    });
+
+    res.send(imgOptimizedFriendsList);
   } catch (error) {
     console.log("something went wrong : ", error.message);
     res.send("Something went wrong : " + error.message);
@@ -37,10 +69,41 @@ followRouter.get("/following", userAuth, async (req, res) => {
       })
       .populate({
         path: "follower",
-        select: "firstName lastName photo",
+        select: "firstName lastName",
       })
       .lean();
-    res.send(following);
+    const imgOptimizedFriendsList = following.map((item) => {
+      if (
+        !item.followee ||
+        !item.followee.photo ||
+        !item.followee.photo.public_id
+      ) {
+        console.log("images not optmizied as needed info was missing");
+        return item;
+      }
+
+      console.log("begin optmizing images");
+
+      const optimizedProfileImg = optimizedImg(
+        item.followee.photo.public_id,
+        50
+      );
+
+      console.log("Image optimized!");
+
+      return {
+        ...item,
+        followee: {
+          ...item.followee,
+          photo: {
+            ...item.followee.photo,
+            url: optimizedProfileImg,
+          },
+        },
+      };
+    });
+
+    res.send(imgOptimizedFriendsList);
   } catch (error) {
     console.log("something went wrong : ", error.message);
     res.send("Something went wrong : " + error.message);
@@ -55,14 +118,46 @@ followRouter.get("/followers/:userId", userAuth, async (req, res) => {
     })
       .populate({
         path: "followee",
-        select: "firstName lastName photo",
+        select: "firstName lastName",
       })
       .populate({
         path: "follower",
         select: "firstName lastName photo",
       })
       .lean();
-    res.send(followers);
+
+    const imgOptimizedFriendsList = followers.map((item) => {
+      if (
+        !item.follower ||
+        !item.follower.photo ||
+        !item.follower.photo.public_id
+      ) {
+        console.log("images not optmizied as needed info was missing");
+        return item;
+      }
+
+      console.log("begin optmizing images");
+
+      const optimizedProfileImg = optimizedImg(
+        item.follower.photo.public_id,
+        50
+      );
+
+      console.log("Image optimized!");
+
+      return {
+        ...item,
+        follower: {
+          ...item.follower,
+          photo: {
+            ...item.follower.photo,
+            url: optimizedProfileImg,
+          },
+        },
+      };
+    });
+
+    res.send(imgOptimizedFriendsList);
   } catch (error) {
     console.log("something went wrong : ", error.message);
     res.send("Something went wrong : " + error.message);
@@ -81,10 +176,41 @@ followRouter.get("/following/:userId", userAuth, async (req, res) => {
       })
       .populate({
         path: "follower",
-        select: "firstName lastName photo",
+        select: "firstName lastName",
       })
       .lean();
-    res.send(following);
+    const imgOptimizedFriendsList = following.map((item) => {
+      if (
+        !item.followee ||
+        !item.followee.photo ||
+        !item.followee.photo.public_id
+      ) {
+        console.log("images not optmizied as needed info was missing");
+        return item;
+      }
+
+      console.log("begin optmizing images");
+
+      const optimizedProfileImg = optimizedImg(
+        item.followee.photo.public_id,
+        50
+      );
+
+      console.log("Image optimized!");
+
+      return {
+        ...item,
+        followee: {
+          ...item.followee,
+          photo: {
+            ...item.followee.photo,
+            url: optimizedProfileImg,
+          },
+        },
+      };
+    });
+
+    res.send(imgOptimizedFriendsList);
   } catch (error) {
     console.log("something went wrong : ", error.message);
     res.send("Something went wrong : " + error.message);
