bb48582d0d70f1d7d2c9fa098e4f85467421ba47
Chirag <chiragbuddy5@gamil.com>
2025-09-04 21:37:45 +0530

    added controllers for profile router

---
+++ src/controller/ProfileController.js
@@ -0,0 +1,215 @@
+const { cloudinary } = require("../utils/cloudinaryConfig");
+const { optimizedImg } = require("../utils/helperFunctions");
+const User = require("../models/user");
+const { validationProfileEdit } = require("../utils/validation");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const multer = require("multer");
+const fs = require("fs/promises");
+
+const UserProfileDetailsController = (req, res) => {
+  try {
+    const user = req.user;
+
+    const optimizedProfileImg = optimizedImg(user.photo.public_id, 400);
+
+    const photoUrl = optimizedProfileImg || req.user.photo?.url;
+
+    res.json({
+      ...req.user,
+      photo: { url: photoUrl },
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Cannot fetch user profile" });
+  }
+};
+
+const ProfileEditController = async (req, res) => {
+  try {
+    if (!validationProfileEdit(req)) {
+      throw new Error("Invalid edit request");
+    }
+
+    const user = await User.findByIdAndUpdate(
+      { _id: req.user._id },
+      {
+        ...req.body,
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+
+    res.send("Profile Updated successfully :)");
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+
+const PasswordEditController = async (req, res) => {
+  try {
+    const { currentPassword, newPassword } = req.body;
+
+    if (!validator.isStrongPassword(newPassword)) {
+      throw new Error("Password is not strong");
+    }
+
+    console.log("input validated");
+
+    const verifyCurrentPassword = await bcrypt.compare(
+      currentPassword,
+      req.user.password
+    );
+
+    if (!verifyCurrentPassword) {
+      res.send("Incorrect Password");
+    }
+
+    console.log("current password validated");
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    console.log(hashedPassword);
+
+    const user = await User.findByIdAndUpdate(
+      { _id: req.user._id },
+      {
+        password: hashedPassword,
+        passwordChangedAt: new Date(),
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+    });
+
+    res.status(200).send(user);
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+
+const ProfileDetailsController = async (req, res) => {
+  try {
+    const { profileId } = req.params;
+    if (typeof profileId !== "string") {
+      res.send("Invalid Profile");
+    } else {
+      const profileDetail = await User.findById({
+        _id: profileId,
+      });
+      if (!profileDetail) {
+        res.send("User does not exist");
+      } else {
+        const optimizedProfileImg = cloudinary.url(
+          profileDetail.photo.public_id,
+          {
+            fetch_format: "auto",
+            quality: "auto",
+            secure: true,
+            width: 400,
+            crop: "limit",
+          }
+        );
+        profileDetail.photo = { url: optimizedProfileImg };
+        res.send(profileDetail);
+      }
+    }
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+
+const UploadController = async (req, res) => {
+  const user = req.user;
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded." });
+  }
+
+  const localFilePath = req.file.path;
+  console.log("Local file path:", localFilePath);
+  try {
+    const DEFAULT_IMAGE_URL = process.env.DEFAULT_PROFILE_IMG_URL;
+    const DEFAULT_IMAGE_ID = process.env.DEFAULT_PROFILE_IMG_ID;
+    console.log("Current image id", user.photo.public_id);
+
+    const hasImage = user.photo.public_id !== DEFAULT_IMAGE_ID;
+
+    if (hasImage) {
+      const deleteImageFromCloudinary = await cloudinary.uploader.destroy(
+        user.photo.public_id
+      );
+      console.log(
+        "Existing user image deleted from cloudinary",
+        deleteImageFromCloudinary
+      );
+    }
+
+    const cloudImg = await cloudinary.uploader.upload(localFilePath, {
+      folder: "profileImg",
+    });
+
+    const updatedUser = await User.findByIdAndUpdate(
+      { _id: user._id },
+      {
+        photo: {
+          url: cloudImg.secure_url,
+          public_id: cloudImg.public_id,
+        },
+      },
+      { new: true, runValidators: true }
+    );
+
+    await fs.unlink(localFilePath);
+
+    if (!updatedUser) {
+      await cloudinary.uploader.destroy(cloudImg.public_id);
+      await User.findByIdAndUpdate(
+        { _id: user._id },
+        {
+          photo: {
+            url: DEFAULT_IMAGE_URL,
+            public_id: DEFAULT_IMAGE_ID,
+          },
+        }
+      );
+      return res.status(404).json({ message: "Unable to update." });
+    }
+
+    console.log("Local file deleted");
+
+    res.status(200).json({
+      message: "Image uploaded and profile updated successfully!",
+      updatedUser,
+      imageUrl: cloudImg.secure_url,
+    });
+  } catch (error) {
+    console.error("Error during image upload or processing:", error);
+
+    try {
+      if (localFilePath) {
+        await fs.unlink(localFilePath);
+      }
+    } catch (unlinkError) {
+      console.error(`Failed to delete local file:`, unlinkError);
+    }
+
+    res.status(500).json({
+      message: "Server error during image upload or profile update",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  UserProfileDetailsController,
+  ProfileEditController,
+  PasswordEditController,
+  ProfileDetailsController,
+  UploadController,
+};
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,224 +1,32 @@
 const express = require("express");
 const userAuth = require("../middlewares/userAuth");
-const { validationProfileEdit } = require("../utils/validation");
-const validator = require("validator");
 const profileRouter = express.Router();
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
-const multer = require("multer");
-const { upload, cloudinary } = require("../utils/cloudinaryConfig");
-const fs = require("fs/promises");
+const { upload } = require("../utils/cloudinaryConfig");
+const {
+  ProfileEditController,
+  PasswordEditController,
+  UserProfileDetailsController,
+  ProfileDetailsController,
+  UploadController,
+} = require("../controller/ProfileController");
 
-profileRouter.get("/profile/view", userAuth, (req, res) => {
-  try {
-    const user = req.user;
-    delete user.password;
+profileRouter.get("/profile/view", userAuth, UserProfileDetailsController);
 
-    const optimizedProfileImg = cloudinary.url(user.photo.public_id, {
-      fetch_format: "auto",
-      quality: "auto",
-      secure: true,
-      width: 400,
-      crop: "limit",
-    });
+profileRouter.post("/profile/edit", userAuth, ProfileEditController);
 
-    user.photo = { url: optimizedProfileImg };
+profileRouter.post("/profile/edit/password", userAuth, PasswordEditController);
 
-    res.send(user);
-  } catch (error) {
-    res.status(400).send("cannot fetch user, please login");
-  }
-});
-
-profileRouter.post("/profile/edit", userAuth, async (req, res) => {
-  try {
-    if (!validationProfileEdit(req)) {
-      throw new Error("Invalid edit request");
-    }
-
-    const user = await User.findByIdAndUpdate(
-      { _id: req.user._id },
-      {
-        ...req.body,
-      },
-      {
-        runValidators: true,
-        new: true,
-      }
-    );
-
-    res.send("Profile Updated successfully :)");
-  } catch (error) {
-    res.send(error.message);
-  }
-});
-
-profileRouter.post("/profile/edit/password", userAuth, async (req, res) => {
-  try {
-    const { currentPassword, newPassword } = req.body;
-
-    if (!validator.isStrongPassword(newPassword)) {
-      throw new Error("Password is not strong");
-    }
-
-    console.log("input validated");
-
-    const verifyCurrentPassword = await bcrypt.compare(
-      currentPassword,
-      req.user.password
-    );
-
-    if (!verifyCurrentPassword) {
-      res.send("Incorrect Password");
-    }
-
-    console.log("current password validated");
-
-    const hashedPassword = await bcrypt.hash(newPassword, 10);
-
-    console.log(hashedPassword);
-
-    const user = await User.findByIdAndUpdate(
-      { _id: req.user._id },
-      {
-        password: hashedPassword,
-        passwordChangedAt: new Date(),
-      },
-      {
-        runValidators: true,
-        new: true,
-      }
-    );
-
-    res.cookie("token", null, {
-      expires: new Date(Date.now()),
-    });
-
-    res.status(200).send(user);
-  } catch (error) {
-    res.send(error.message);
-  }
-});
-
-profileRouter.get("/profile/view/:profileId", userAuth, async (req, res) => {
-  try {
-    const { profileId } = req.params;
-    if (typeof profileId !== "string") {
-      res.send("Invalid Profile");
-    } else {
-      const profileDetail = await User.findById({
-        _id: profileId,
-      });
-      if (!profileDetail) {
-        res.send("User does not exist");
-      } else {
-        const optimizedProfileImg = cloudinary.url(
-          profileDetail.photo.public_id,
-          {
-            fetch_format: "auto",
-            quality: "auto",
-            secure: true,
-            width: 400,
-            crop: "limit",
-          }
-        );
-        profileDetail.photo = { url: optimizedProfileImg };
-        res.send(profileDetail);
-      }
-    }
-  } catch (error) {
-    res.send(error.message);
-  }
-});
+profileRouter.get(
+  "/profile/view/:profileId",
+  userAuth,
+  ProfileDetailsController
+);
 
 profileRouter.post(
   "/profile/addImage",
   userAuth,
   upload.single("file"),
-  async (req, res) => {
-    const user = req.user;
-    if (!req.file) {
-      return res.status(400).json({ message: "No file uploaded." });
-    }
-
-    const localFilePath = req.file.path;
-    console.log("Local file path:", localFilePath);
-    try {
-      console.log("Current image id", user.photo.public_id);
-
-      const hasImage = user.photo.public_id !== "default_placeholder";
-
-      if (hasImage) {
-        const deleteImageFromCloudinary = await cloudinary.uploader.destroy(
-          user.photo.public_id
-        );
-        console.log(
-          "Existing user image deleted from cloudinary",
-          deleteImageFromCloudinary
-        );
-      }
-
-      const cloudImg = await cloudinary.uploader.upload(localFilePath, {
-        folder: "profileImg",
-      });
-
-      const updatedUser = await User.findByIdAndUpdate(
-        { _id: user._id },
-        {
-          photo: {
-            url: cloudImg.secure_url,
-            public_id: cloudImg.public_id,
-          },
-        },
-        { new: true, runValidators: true }
-      );
-
-      if (!updatedUser) {
-        await cloudinary.uploader.destroy(cloudImg.public_id);
-        await User.findByIdAndUpdate(
-          { _id: user._id },
-          {
-            photo: {
-              url: "https://res.cloudinary.com/doknrbhso/image/upload/v1746265741/samples/animals/cat.jpg",
-              public_id: "default_placeholder",
-            },
-          }
-        );
-        return res.status(404).json({ message: "Unable to update." });
-      }
-
-      await fs.unlink(localFilePath);
-
-      console.log(`Local file deleted: ${localFilePath}`);
-
-      res.status(200).json({
-        message: "Image uploaded and profile updated successfully!",
-        updatedUser: updatedUser,
-        imageUrl: cloudImg.secure_url,
-      });
-    } catch (error) {
-      console.error("Error during image upload or processing:", error);
-
-      if (localFilePath) {
-        try {
-          await fs.unlink(localFilePath);
-          console.log(
-            `Error occurred, but local file deleted: ${localFilePath}`
-          );
-        } catch (unlinkError) {
-          console.error(
-            `Failed to delete local file ${localFilePath} during error handling:`,
-            unlinkError
-          );
-        }
-      }
-
-      res.status(500).json({
-        message: "Server error during image upload or profile update",
-        error: error.message,
-      });
-    }
-  }
+  UploadController
 );
 
 module.exports = profileRouter;
